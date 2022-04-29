f103f37568a0665433765e3ab167df36ed4ccfb5
Sean-Ubele <seanjohnreedubeleknuckles@gmail.com>
2022-04-29 12:31:23 -0400

    Spinner.test complete, all tests passing.

--- frontend/components/Spinner.test.js
+++ frontend/components/Spinner.test.js
@@ -1,5 +1,26 @@
+import Spinner from './Spinner' 
+import React from 'react'
+import {render, screen} from '@testing-library/react'
+import '@testing-library/jest-dom'
+
 // Import the Spinner component into this file and test
 // that it renders what it should for the different props it can take.
 test('sanity', () => {
-  expect(true).toBe(true  )
+  expect(true).toBe(true)
 })
+
+const loadingMessage = 'Please Wait...';
+
+test('does not render loadingMessage if props = false', () => {
+  render(<Spinner on={false}/>)
+  const lM = screen.queryByText(loadingMessage)
+  expect(lM).not.toBeInTheDocument()
+})
+
+test('loadingMessage is in the document if props = true', () => {
+  render(<Spinner on={true}/>)
+  const lM = screen.queryByText(loadingMessage)
+  expect(lM).toBeInTheDocument
+})
+
+
